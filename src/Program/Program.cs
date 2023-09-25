3409fad4b4168209ce10971e69f47d07c1a01f59
MHC2005 <mhcedres@gmail.com>
2023-09-25 08:19:22 -0300

    cambios

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,4 @@
-﻿//-------------------------------------------------------------------------
+﻿﻿//-------------------------------------------------------------------------
 // <copyright file="Program.cs" company="Universidad Católica del Uruguay">
 // Copyright (c) Programación II. Derechos reservados.
 // </copyright>
@@ -26,9 +26,11 @@ namespace Full_GRASP_And_SOLID
             recipe.AddStep(new Step(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120));
             recipe.AddStep(new Step(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60));
 
-            AllInOnePrinter printer = new AllInOnePrinter();
-            printer.PrintRecipe(recipe, Destination.Console);
-            printer.PrintRecipe(recipe, Destination.File);
+            IPrinter printer;
+            printer = new ConsolePrinter();
+            printer.PrintTicket(recipe);
+            printer = new FilePrinter();
+            printer.PrintTicket(recipe);
         }
 
         private static void PopulateCatalogs()
